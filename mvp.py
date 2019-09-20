742196c8888f7e4aba0affd2e7ffe83c8e538518
sciencylab <vincent@sciencylab.com>
2019-09-20 18:30:46 +0000

    used pyspark to parallelize the caculation of cycles' values

--- mvp.py
+++ mvp.py
@@ -1,5 +1,13 @@
 import psycopg2
 from read_config import read_config
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+
+## Creating spark context and session
+sc = SparkContext.getOrCreate()
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+####***********************************************
+
 
 def connect_to_psql(filename):
     """
@@ -23,6 +31,36 @@ def create_table(conn, table_name, schema_string):
 
 
 
+def cycle_ratio(cycle, edges):
+    res = 1.0
+    for x, y in cycle:
+        res = res * edges[x][y]
+    return res
+
+
+def read_price_and_update(filename, edges):
+    """
+    Takes in filename (of form AB.csv), where 1st letter refers to base currency and 2nd letter refers to the quoted currency.
+
+    For example, if the price listed in AB.csv is 1.3, then that means B is worth 1.3 A.
+
+    Returns a triplet: (base currency, quoted currency, price)
+    """
+    tmp = filename[:-3]
+    base, quoted = tmp[0].strip(), tmp[1].strip()
+
+    with open(filename, 'r') as file:
+        file.readline()
+        price = float(file.readline().split(',')[1].strip())
+        edges[base][quoted] = price
+        if abs(price) > 1.0e-8:
+            edges[quoted][base] = 1.0 / price
+        elif price > 0:
+            edges[quoted][base] = 1.0e8
+        else:
+            edges[quoted][base] = -1.0e8
+        return (base, quoted, price)
+
 
 
 
@@ -41,34 +79,39 @@ def main():
     # cursor.close()
     # conn.close()
     
-    ## read price from files
-    print(read_open('AB.csv'))
-    print(read_open('BC.csv'))
-    print(read_open('AD.csv'))
-    print(read_open('AC.csv'))
-    print(read_open('CD.csv'))
 
+    ## Initializing nodes and edges
+    nodes = {'A': 1, 'B': 2, 'C': 3, 'D': 4}
+    edges = {}
+    for node in nodes: edges[node] = {}
+    
+    ## Reading price from files
+    print(read_price_and_update('AB.csv', edges))
+    print(read_price_and_update('BC.csv', edges))
+    print(read_price_and_update('AD.csv', edges))
+    print(read_price_and_update('AC.csv', edges))
+    print(read_price_and_update('CD.csv', edges))
+    
 
-    ## cycles
+    ## Defining cycles
     cycle1 = [('A','B'), ('B','C'), ('C','A')]
     cycle2 = [('A','B'), ('B','C'), ('C','D'), ('D','A')]
     cycle3 = [('A','C'), ('C','D'), ('D','A')]
     cycles = [cycle1, cycle2, cycle3]
 
-def read_open(filename):
-    """
-    Takes in filename (of form AB.csv), where 1st letter refers to base currency and 2nd letter refers to the quoted currency.
 
-    For example, if the price listed in AB.csv is 1.3, then that means B is worth 1.3 A.
+    ## Calculate value of the cycles
+    calculate = lambda x: cycle_ratio(x, edges)
+    A = sc.parallelize(cycles)
+    B = A.map(calculate)
+    C = B.collect()
 
-    Returns a triplet: (base currency, quoted currency, price)
-    """
-    tmp = filename[:-3]
-    base, quoted = tmp[0], tmp[1]
+    print('\n-----------\n')
+    print(type(C))
+    print(C)
+    print('\n-----------\n')
 
-    with open(filename, 'r') as file:
-        file.readline()
-        price = float(file.readline().split(',')[1].strip())
-        return (base, quoted, price)
+
 
 main()
+    
